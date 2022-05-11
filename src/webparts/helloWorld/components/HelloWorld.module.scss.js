52ecfc545443193b218be5ecc47b3dca914adbb9
Ajith kumar <ajidad3@gmail.com>
2022-05-11 12:12:36 +0530

    SPFX added

---
+++ src/webparts/helloWorld/components/HelloWorld.module.scss.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/* tslint:disable */
+require('./HelloWorld.module.css');
+var styles = {
+    helloWorld: 'helloWorld_28535e31',
+    container: 'container_28535e31',
+    row: 'row_28535e31',
+    column: 'column_28535e31',
+    'ms-Grid': 'ms-Grid_28535e31',
+    title: 'title_28535e31',
+    subTitle: 'subTitle_28535e31',
+    description: 'description_28535e31',
+    button: 'button_28535e31',
+    label: 'label_28535e31',
+};
+exports.default = styles;
+/* tslint:enable */ 
+//# sourceMappingURL=HelloWorld.module.scss.js.map
